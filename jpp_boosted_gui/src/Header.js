30789a2de3488e47c008edb5a7fe94a52d122a15
QualmandDriven <QualmandDriven@outlook.com>
2018-01-17 20:46:11 +0100

    Made header and footer more responsive because of new link

--- jpp_boosted_gui/src/Header.js
+++ jpp_boosted_gui/src/Header.js
@@ -11,12 +11,14 @@ class Header extends React.Component {
                 <div className="header">
                     <div className="header-name">
                         <Link className="header-home-link" to="/">JPP-Boosted!</Link>
-                        <Link className="ml-2 header-link" to="/projects">Alle Projekte</Link>
                     </div>
                     <div className="header-logo">
                         <a target="blank" href="https://jp-performance.de">
                             <img className="logo" src={logo} alt="logo" />
                         </a>
+                    </div>
+                    <div className="header-name">
+                        <Link className="ml-2 header-link" to="/projects">Alle Projekte</Link>
                     </div>
                 </div>
             </header>
